fc68b630ec6d6be75ae8ef9bdbde411ed1ed477f
Abdullah Akkaya <abdullahhakkaya41@gmail.com>
2025-05-18 01:26:15 +0300

    Upload to GitHub

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please enter a positive number to calculate its factorial : ");
+        ulong number = Convert.ToUInt64(Console.ReadLine());
+        ulong sum = 1;
+
+        if (number > 0)
+        {
+            for (ulong i = number; i > 0; i--)
+            {
+                sum *= i;
+            }
+            Console.WriteLine("Calculation result : " + sum);
+        }
+        else
+        {
+            Console.WriteLine("You have entered an invalid input.");
+        }
+        Console.ReadLine();
+    }
+}
